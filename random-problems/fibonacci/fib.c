1253d2b5da9c347526a0d0060e73196afa5d32ad
Swarnim B <swarnim14.9@hotmail.com>
2024-01-21 14:18:16 +0530

    cmt

--- random-problems/fibonacci/fib.c
+++ random-problems/fibonacci/fib.c
@@ -3,13 +3,9 @@
 int recursive_fibonacci(int n)
 {
     if (n <= 1)
-    {
         return n;
-    }
     else
-    {
         return recursive_fibonacci(n - 1) + recursive_fibonacci(n - 2);
-    }
 }
 
 int main()
@@ -17,7 +13,5 @@ int main()
     printf("Hello, World\n");
     int n = 10;
     for (int i = 0; i < n; i++)
-    {
         printf("Fibonacci(%d): %d\n", i, recursive_fibonacci(i));
-    }
 }
